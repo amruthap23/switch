bdb6a7f5706805cb2b2de323f413a873f36bba13
amruthap23 <85816788+amruthap23@users.noreply.github.com>
2021-06-22 11:20:09 +0530

    Created switch.c
    Select your food menu form 1-5

---
+++ Switch.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main() {
+    char x;
+    printf("Menu items: \n\n
+            1. Pizza,Rs239 \n
+            2. Burger,Rs129 \n
+            3. Pasta,Rs179 \n
+            4. French fries,Rs99 \n
+            5. Sandwich,Rs149 \n");
+    printf("Enter random number from 1-5 : ");
+    scanf("%d", &x);
+    switch(x)
+    {
+        case 1 :
+            printf("Food item: Pizza \n Price: Rs239 \n");
+            break;
+
+        case 2 :
+            printf("Food item: Burger \n Price: Rs129 \n");
+            break;
+
+        case 3 :
+            printf("Food item: Pasta \n Price: Rs179 \n");
+            break;
+
+        case 4 :
+            printf("Food item: French fries \n Price: Rs99 \n");
+            break;
+
+        case 5 :
+            printf("Food item: sandwich \n Price: Rs149 \n");
+            break;
+
+        default:
+            printf("Error! Number doesn't exist from 1-5");
+    }
+
+    return 0;
+}
+
+    
+
+
+    
